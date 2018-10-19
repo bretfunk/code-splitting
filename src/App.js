97a31a33058fc2e53af9055104bc7ff1e1ffb9eb
BFunk <bret.e.funk@gmail.com>
2018-10-19 11:45:43 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Page1 from "./components/Page1";
+import AsyncComponent from "./components/AsyncComponent";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      route: "page1",
+      component: null
+    };
+  }
+
+  onRouteChange = route => {
+    this.setState({ route });
+  };
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    const { route } = this.state;
+    if (route === "page1") {
+      return <Page1 onRouteChange={this.onRouteChange} />;
+    } else if (route === "page2") {
+      const AsyncPage2 = AsyncComponent(() => import("./components/Page2"));
+      return <AsyncPage2 onRouteChange={this.onRouteChange} />;
+    } else if (route === "page3") {
+      const AsyncPage3 = AsyncComponent(() => import("./components/Page3"));
+      return <AsyncPage3 onRouteChange={this.onRouteChange} />;
+    }
   }
 }
 
